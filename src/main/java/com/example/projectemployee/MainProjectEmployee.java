6b775462f54b0b408689ddcefdd5c47e5f91f71c
HoThiVu <89339469+HoThiVu@users.noreply.github.com>
2022-07-06 17:27:04 +0700

    alert

--- src/main/java/com/example/projectemployee/MainProjectEmployee.java
+++ src/main/java/com/example/projectemployee/MainProjectEmployee.java
@@ -9,15 +9,13 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 //import static java.rmi.server.LogStream.log;
 
@@ -147,6 +145,31 @@ public class MainProjectEmployee extends Application {
                     new Label(" " + emloyeeList.get(i).getQuocTich()), new Label(" " + emloyeeList.get(i).getDanToc()));
             int finalI = i;
             btnDel.setOnAction(e -> {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Cofirmation");
+                alert.setHeaderText("Alert Information");
+                alert.setContentText("choose your option");
+
+                ButtonType buttonTypeYes = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+                ButtonType buttonTypeNo = new ButtonType("No", ButtonBar.ButtonData.NO);
+                ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+                alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo, buttonTypeCancel);
+
+                Optional<ButtonType> result = alert.showAndWait();
+
+                if (result.get()== buttonTypeYes)
+                    System.out.println("Code for yes");
+                else if (result.get().getButtonData() == ButtonBar.ButtonData.NO)
+                    System.out.println("Code for no");
+                else
+                    System.out.println("Code for cancel");
+                String message = result.get().getText();
+                Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+                alert1.setTitle("Information");
+                alert1.setHeaderText("Notification");
+                alert1.setContentText(message);
+                alert1.show();
                 db.deleteEmployee(emloyeeList.get(finalI).getMaNV());
                 rootGridPane.getChildren().clear();
                 getDisplayEmployees(vBox, db, rootGridPane, formGridPane);
